067e12d1cd08784c7ba25950ee03e7531c83884d
Degenerate <82691315+Degenerate0001@users.noreply.github.com>
2021-10-28 19:04:31 -0500

    only corrosion

--- start.js
+++ start.js
@@ -1,13 +1,11 @@
 const
     corrosion = require('./corrosion'),
-    smokeproxy = require('./Smoke/lib/smoke')
     path = require('path'),
     config = require('./config.json'),
     fs = require('fs'),
     https = require('https'),
     http = require('http'),
     btoa = e => new Buffer.from(e).toString("base64"),
-    prefix = '/web/'
     ssl = {
         key: fs.readFileSync(path.join(__dirname, '/ssl/default.key')),
         cert: fs.readFileSync(path.join(__dirname, '/ssl/default.crt')),
@@ -22,20 +20,9 @@ const
         requestMiddleware: [
             corrosion.middleware.blacklist((config.blacklist || []), 'This page has been blocked!'),
         ],
-    })
-    smoke = new smokeproxy(prefix, {
-        docTitle: 'Smoke'
-    }),
+    });
     app = require('express')().use(require('express').static(path.normalize(__dirname + '/public/'), {extensions: ['html']})).use((req, res) => {
     if (req.url.startsWith(proxy.prefix)) return proxy.request(req,res);
-    else if (req.url.startsWith(prefix + "gateway")) {
-      res.redirect(prefix + btoa(req.query.url))
-    } else if (req.url.startsWith(prefix)) {
-      return smoke.request(req, res)
-    } else res.status(404, res.send(error))
-    }).post('*', (req, res) => {
-        if (req.url.startsWith(prefix)) return smoke.post(req, res)
-    });
 
 proxy.bundleScripts();
 
