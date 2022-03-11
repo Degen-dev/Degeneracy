387696614840f34e0b8ae15b0412629747416676
Degen-dev <degen0001@protonmail.com>
2022-03-10 22:27:09 -0600

    im doing my best lol

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', async event => {
         scope: __uv$config.prefix
     }).then(() => {
         let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        if (!isUrl(url)) url = 'https://searx.degenerate.info/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
 
